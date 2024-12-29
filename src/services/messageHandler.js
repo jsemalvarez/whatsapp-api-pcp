7c4a9680023c1498941102ed7766cb89999a1bde
jsemalvarez <jsemalvarez@gmail.com>
2024-12-29 11:32:36 -0300

    feat: sendWelcomeMessage

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -4,11 +4,28 @@ import config from '../config/env.js';
 class MessageHandler {
   async handleIncomingMessage(message) {
     if (message?.type === 'text') {
-      const response = `Echo: ${message.text.body}`;
       const DESTINATION_PHONE = (config.NODE_ENV === 'PROD')? message.from : config.DEV_PHONE;
-      await whatsappService.sendMessage(DESTINATION_PHONE, response, message.id);
+      const incommingMessage = message.text.body.toLowerCase().trim();
+
+      if(this.isGreeting(incommingMessage)){
+        await this.sendWelcomeMessage(DESTINATION_PHONE, message.id)
+      }else{
+        const response = `Echo: ${message.text.body}`;
+        await whatsappService.sendMessage(DESTINATION_PHONE, response, message.id);
+      }
+
       await whatsappService.markAsRead(message.id);
     }
+  }
+
+  isGreeting(message){
+    const greetings = ['hola', 'buenas'];
+    return greetings.includes(message)
+  }
+
+  async sendWelcomeMessage(to, messageId){
+    const welcomeMessage = 'Hola. Soy el asistene virtual de PCP. ¿En qué puedo ayudarte?';
+    await whatsappService.sendMessage(to, welcomeMessage, messageId)
   }
 }
 
