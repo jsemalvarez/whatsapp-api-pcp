f0d99b01f1262196e03dc77dd68c5638efd42c7d
jsemalvarez <jsemalvarez@gmail.com>
2024-12-29 11:54:02 -0300

    feat: getSenderName

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -2,13 +2,13 @@ import whatsappService from './whatsappService.js';
 import config from '../config/env.js';
 
 class MessageHandler {
-  async handleIncomingMessage(message) {
+  async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
       const DESTINATION_PHONE = (config.NODE_ENV === 'PROD')? message.from : config.DEV_PHONE;
       const incommingMessage = message.text.body.toLowerCase().trim();
 
       if(this.isGreeting(incommingMessage)){
-        await this.sendWelcomeMessage(DESTINATION_PHONE, message.id)
+        await this.sendWelcomeMessage(DESTINATION_PHONE, message.id, senderInfo)
       }else{
         const response = `Echo: ${message.text.body}`;
         await whatsappService.sendMessage(DESTINATION_PHONE, response, message.id);
@@ -19,12 +19,19 @@ class MessageHandler {
   }
 
   isGreeting(message){
-    const greetings = ['hola', 'buenas'];
+    const greetings = ['hola','hola ','hola,','hola.', 'buenas'];
     return greetings.includes(message)
   }
 
-  async sendWelcomeMessage(to, messageId){
-    const welcomeMessage = 'Hola. Soy el asistene virtual de PCP. ¿En qué puedo ayudarte?';
+  getSenderName(senderInfo){
+    const name = senderInfo.profile?.name || senderInfo.wa_id || '';
+    const firstName = name.split(' ')[0];
+    return firstName
+  }
+
+  async sendWelcomeMessage(to, messageId, senderInfo){
+    const name = this.getSenderName(senderInfo);
+    const welcomeMessage = `Hola ${name}. te saluda el asistene virtual de PCP. ¿En qué puedo ayudarte?`;
     await whatsappService.sendMessage(to, welcomeMessage, messageId)
   }
 }
