4b48d31d279e5559813f4e11694cae97a53e003d
jsemalvarez <jsemalvarez@gmail.com>
2025-01-11 11:18:17 -0300

    refactor: add some logs

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -44,8 +44,9 @@ class MessageHandler {
       await whatsappService.markAsRead(message.id);
     }else{
       console.log(message)
-      // const DESTINATION_PHONE = (config.NODE_ENV === 'PROD')? message.from : config.DEV_PHONE;
-      // const message= `Perdon, pero no estoy programado para responder el mesaje que enviaste. Recauerda que solo soy un asistente que puede respoder a las opciones por las cuales que voy guiando.\nPara ver el menú principal envia menu`
+      const DESTINATION_PHONE = (config.NODE_ENV === 'PROD')? message.from : config.DEV_PHONE;
+      console.log({DESTINATION_PHONE})
+      // const message= `Perdon, pero no estoy programado para responder el mesaje que enviaste. Recauerda que solo soy un asistente que puede respoder a las opciones por las cuales que voy guiando.\nPara ver el menú principal envia *menu*`
       // await whatsappService.sendMessage(DESTINATION_PHONE, message);
     }
 
